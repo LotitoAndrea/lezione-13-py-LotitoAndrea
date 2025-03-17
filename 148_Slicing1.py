18efb8b059d2de01b6ba90c7bcde9cd446096cba
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-03-17 11:42:04 +0100

    modifiche al 148

--- 148_Slicing1.py
+++ 148_Slicing1.py
@@ -1,5 +1,5 @@
 stringa = input("inserisci una stringa: ")
-if len(stringa) < 4:
+if len(stringa) <= 4:
     print("Numero caratteri insufficienti")
 else:
-    print(stringa[1:4])
+    print(stringa[1:5])
